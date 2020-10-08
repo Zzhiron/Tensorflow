121375cdeac74a1e8cb7697e17e8b4ca343a70f1
Zzhiron <41948078+Zzhiron@users.noreply.github.com>
2020-10-08 10:46:02 +0800

    Update RecogDll.cpp

--- ClassificationPipeline/C++Projects/tensorflow_product/RecogDll.cpp
+++ ClassificationPipeline/C++Projects/tensorflow_product/RecogDll.cpp
@@ -3,15 +3,15 @@
 
 void RecogDll::init_model(const int mode)
 {
-    _ex_module_vvc.initialize(mode, 512, 640, 3);
+    _ex_module.initialize(mode, 512, 640, 3);
 }
 
 int RecogDll::predict(const string& image_path)
 {
     Mat img_src = imread(image_path, -1);
     Mat img_resized;
-    resize(img_src, img_resized, Size(640, 512));
-    int result = _ex_module_vvc.predict(img_resized);
+    resize(img_src, img_resized, Size(512, 512));
+    int result = _ex_module.predict(img_resized);
    
     return result;
 }
